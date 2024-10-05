bd5ae793be279466ff18342e950cfba09848caa6
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2024-10-05 12:48:56 +0300

    Add project files.

---
+++ OneMicroservice.API/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Bus.Shared;
+using MassTransit;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OneMicroservice.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController(IPublishEndpoint publishEndpoint) : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> CreateUser()
+        {
+            //  user to create;
+
+
+            await publishEndpoint.Publish(new UserCreatedEvent(Guid.NewGuid(), "ahmet@outlook.com", "555 555 55 55"));
+            return Ok();
+        }
+    }
+}
